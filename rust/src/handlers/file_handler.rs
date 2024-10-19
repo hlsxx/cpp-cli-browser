e0788010a5df11ceaea6b3f2280b1573bb67b743
hlsxx <holespato@gmail.com>
2024-10-19 20:21:43 +0200

    file_handler

--- rust/src/handlers/file_handler.rs
+++ rust/src/handlers/file_handler.rs
@@ -1,3 +1,30 @@
-use std::path::PathBuf;
+use std:: path::PathBuf;
+use std::fs::File;
 
-pub struct FileHandler(PathBuf);
+pub struct FileHandler {
+  input_file_path: PathBuf,
+  files: Vec<PathBuf>
+}
+
+impl FileHandler {
+
+  pub fn new(input_file_path: PathBuf) -> Self {
+    Self {
+      input_file_path,
+      files: Vec::new()
+    }
+  }
+
+  pub fn init(&mut self) {
+    self.search_files(self.input_file_path.clone());
+  }
+
+  pub fn search_files(&mut self, input_file_path: PathBuf) {
+    if input_file_path.is_file() {
+      self.files.push(input_file_path);
+    } else {
+      // self.search_files(input_file_path);
+    }
+  }
+
+}
